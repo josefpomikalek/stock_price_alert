5fd9fbf8d799bd5daa23c1b44dd21ca1d3293bce
josefpomikalek <josef_pomikalek@yahoo.com>
2024-10-07 16:10:08 +0200

    Get yesterday's closing stock price.

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import requests
+from creds import STOCK_API_KEY
+
 STOCK_NAME = "NVDA"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 
+# Get yesterday's closing stock price.
 
+stock_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK_NAME,
+    "apikey": STOCK_API_KEY
+}
+
+response = requests.get(STOCK_ENDPOINT, params=stock_params)
+data = response.json()["Time Series (Daily)"]
+data_list = [value for (key, value) in data.items()]
+yesterday_data = data_list[0]
+yesterday_closing_price = yesterday_data["4. close"]
+print(yesterday_closing_price)
